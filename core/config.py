575f4cf4a4f5e49a50bb5a26465871a31bfa8b84
GuranT <105738274+GuranT@users.noreply.github.com>
2025-11-25 07:37:13 +0800

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,13 +1,18 @@
 from pydantic_settings import BaseSettings
 from typing import List
+import json
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
     BLOCKCYPHER_TOKEN: str
-    ADMIN_IDS: List[int] = []
+    ADMIN_IDS: str = "[]"
     BASE_URL: str = ""
     DATABASE_URL: str = "sqlite+aiosqlite:///dev.db"
 
     model_config = {"env_file": ".env"}
+
+    @property
+    def admin_ids_list(self) -> List[int]:
+        return json.loads(self.ADMIN_IDS)
 
 settings = Settings()
